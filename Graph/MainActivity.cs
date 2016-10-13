60d06fa3c503e63e68fb151874151c4e486503a0
Kimserey Lam <kimserey.lam@gmail.com>
2016-10-13 08:55:40 +0100

    transformed to xamarin forms app

--- Graph/MainActivity.cs
+++ Graph/MainActivity.cs
@@ -1,11 +1,27 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Core;
+using Xamarin.Forms.Platform.Android;
 
 namespace Graph
 {
 	[Activity(Label = "Graph", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	public class MainActivity : FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+			FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
+
+			base.OnCreate(savedInstanceState);
+			Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			this.LoadApplication(new App());
+		}
+	}
+
+	public class GraphAtivity : Activity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
